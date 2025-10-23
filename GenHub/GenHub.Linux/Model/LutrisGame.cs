7d4ae8c37b101912b29c344f74c9d3278ec4dbf9
Kian Kazemzade <lordkian2000@gmail.com>
2025-10-23 22:57:05 +0200

    Update property summaries in LutrisGame class

--- GenHub/GenHub.Linux/Model/LutrisGame.cs
+++ GenHub/GenHub.Linux/Model/LutrisGame.cs
@@ -6,48 +6,48 @@ namespace GenHub.Linux.Model;
 public class LutrisGame
 {
     /// <summary>
-    /// The unique identifier for the game within Lutris.
+    /// Gets the unique identifier for the game within Lutris.
     /// </summary>
     public int id { get; set; } = 0;
 
     /// <summary>
-    /// slug of the game (similar to name).
+    /// Gets slug of the game (similar to name).
     /// </summary>
     public string slug { get; set; } = string.Empty;
 
     /// <summary>
-    /// name of the game.
+    /// Gets name of the game.
     /// </summary>
     public string name { get; set; } = string.Empty;
 
     /// <summary>
-    /// The runner used to launch the game (e.g., "wine", "steam", "dosbox").
+    /// Gets the runner used to launch the game (e.g., "wine", "steam", "dosbox").
     /// </summary>
     public string runner { get; set; } = string.Empty;
 
     /// <summary>
-    /// The runner used to launch the game (e.g., "wine", "Linux").
+    /// Gets the platform used to launch the game (e.g., "wine", "Linux").
     /// </summary>
     public string platform { get; set; } = string.Empty;
 
    /// <summary>
-    /// The official release year of the game, as sourced from Lutris metadata.
+    /// Gets the official release year of the game, as sourced from Lutris metadata.
     /// </summary>
     public int year { get; set; } = 0;
 
     /// <summary>
-    /// The local installation directory path of the game.
+    /// Gets the local installation directory path of the game.
     /// in case of zero hour it will be EA App launcher.
     /// </summary>
     public string directory { get; set; } = string.Empty;
 
     /// <summary>
-    /// The total time the user has spent playing the game.
+    /// Gets the total time the user has spent playing the game.
     /// </summary>
     public string playtime { get; set; } = string.Empty;
 
     /// <summary>
-    /// The timestamp indicating when the game was last played.
+    /// Gets the timestamp indicating when the game was last played.
     /// </summary>
     public string lastplayed { get; set; } = string.Empty;
 }
