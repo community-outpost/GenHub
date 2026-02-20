8fa96bd781335b975941b259e57f5ebe7e454a68
mnoserat <mnoserat1@gmail.com>
2026-02-21 02:03:58 +0300

    feat(fix): rephrase the AI authorship comment

--- GenHub/GenHub.Tests/GenHub.Tests.Core/Features/Content/CSVDiscovererTests.cs
+++ GenHub/GenHub.Tests/GenHub.Tests.Core/Features/Content/CSVDiscovererTests.cs
@@ -14,7 +14,20 @@ using Xunit;
 
 namespace GenHub.Tests.Core.Features.Content;
 
-// the whole file is made by AI
+// Unit tests for CSVDiscoverer. the whole file is made by AI
+// Copyright (C) 2026  GenHub & The Super Hackers
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program.  If not, see <https://www.gnu.org/licenses/>.
 
 /// <summary>
 /// Unit tests for <see cref="CSVDiscoverer"/>.
