1986faccde11b8c2b66b6b9e92003e613c9f8ae5
undead2146 <undead2146@github.com>
2026-08-18 00:44:39 +0000

    fix(modbuilder): address review comments, fix process timeouts and remove benchmarks

--- GenHub/GenHub/Features/GameProfiles/Infrastructure/GameProcessManager.cs
+++ GenHub/GenHub/Features/GameProfiles/Infrastructure/GameProcessManager.cs
@@ -1486,7 +1486,7 @@ public class GameProcessManager(
     {
         try
         {
-            process.WaitForExit();
+            process.WaitForExit(3000);
         }
         catch (Exception ex)
         {
