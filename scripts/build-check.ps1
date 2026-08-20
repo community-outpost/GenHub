8006fe46fd849c28978ed0414b10909b159abf38
undead2146 <mustafa2146@live.be>
2026-08-20 15:57:28 +0200

    feat(ui): redesign header navigation tabs to centered pills and move info/settings to titlebar

---
+++ scripts/build-check.ps1
@@ -0,0 +1,47 @@
+﻿param (
+    [string]$Project = "GenHub/GenHub.sln",
+    [ValidateSet("check", "build", "restore")]
+    [string]$Mode = "check"
+)
+
+$mutexName = "Global\GenHubBuildLock"
+$mutex = New-Object System.Threading.Mutex($false, $mutexName)
+
+try {
+    $hasHandle = $false
+    try {
+        $hasHandle = $mutex.WaitOne(60000, $false)
+    } catch [System.Threading.AbandonedMutexException] {
+        $hasHandle = $true
+    }
+
+    if (-not $hasHandle) {
+        Write-Error "Timed out waiting for build mutex."
+        exit 1
+    }
+
+    $targetPath = $Project
+    if (-not (Test-Path $targetPath)) {
+        if (Test-Path "GenHub/$Project") {
+            $targetPath = "GenHub/$Project"
+        }
+    }
+
+    switch ($Mode) {
+        "check" {
+            dotnet build $targetPath -c Debug -maxcpucount:2 /nologo
+        }
+        "build" {
+            dotnet build $targetPath -c Release -maxcpucount:2 /nologo
+        }
+        "restore" {
+            dotnet restore $targetPath /nologo
+        }
+    }
+}
+finally {
+    if ($hasHandle) {
+        $mutex.ReleaseMutex()
+    }
+    $mutex.Dispose()
+}
