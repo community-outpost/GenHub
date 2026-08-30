329a616d0749d7a32a86df4dd70124d944f6654f
undead2146 <undead2146@github.com>
2026-08-30 18:55:01 +0000

    fix(tools): address code review feedback on replay manager and dependencies

--- GenHub/GenHub/Features/GameInstallations/GameInstallationService.cs
+++ GenHub/GenHub/Features/GameInstallations/GameInstallationService.cs
@@ -807,7 +807,9 @@ IUserSettingsService? userSettingsService = null) : IGameInstallationService, ID
                     Name = matchingManifest.Name,
                     WorkingDirectory = gamePath,
                     ExecutablePath = exePath,
-                    PublisherType = installType,
+                    PublisherType = installation.InstallationType == GameInstallationType.Retail
+                        ? PublisherInfoConstants.Retail.Name
+                        : installType,
                     GameType = gameType,
                     InstallationId = installation.Id,
                     Version = matchingManifest.Version,
