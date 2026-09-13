8df1758adab8d87facdb6931960ae2fe2d24a39f
undead2146 <undead2146@github.com>
2026-09-13 15:21:18 +0000

    fix(genhotkeys): resolve profile flyout UI transparency and dropdown sync issues

--- GenHub/GenHub/Features/Tools/GenHotkeys/Views/GenHotkeysView.axaml.cs
+++ GenHub/GenHub/Features/Tools/GenHotkeys/Views/GenHotkeysView.axaml.cs
@@ -20,6 +20,47 @@ public partial class GenHotkeysView : UserControl
     {
         InitializeComponent();
         AddHandler(KeyDownEvent, OnRootKeyDown, RoutingStrategies.Tunnel);
+        AttachedToVisualTree += (_, _) => SetupFlyouts();
+    }
+
+    private void SetupFlyouts()
+    {
+        var newProfileBtn = this.FindControl<Button>("NewProfileButton");
+        if (newProfileBtn?.Flyout is Flyout newFlyout)
+        {
+            newFlyout.Opened += (_, _) =>
+            {
+                if (newFlyout.Content is Control content)
+                {
+                    content.DataContext = DataContext;
+                }
+
+                var tb = this.FindControl<TextBox>("NewProfileTextBox");
+                tb?.Focus();
+                tb?.SelectAll();
+            };
+        }
+
+        var renameProfileBtn = this.FindControl<Button>("RenameProfileButton");
+        if (renameProfileBtn?.Flyout is Flyout renameFlyout)
+        {
+            renameFlyout.Opened += (_, _) =>
+            {
+                if (renameFlyout.Content is Control content)
+                {
+                    content.DataContext = DataContext;
+                }
+
+                if (DataContext is GenHotkeysViewModel vm && vm.SelectedProfile != null)
+                {
+                    vm.RenameProfileText = vm.SelectedProfile.Name;
+                }
+
+                var tb = this.FindControl<TextBox>("RenameProfileTextBox");
+                tb?.Focus();
+                tb?.SelectAll();
+            };
+        }
     }
 
     private void OnKeyButtonClicked(object? sender, RoutedEventArgs e)
@@ -74,32 +115,41 @@ public partial class GenHotkeysView : UserControl
         }
     }
 
-    private void OnFlyoutTextBoxKeyDown(object? sender, KeyEventArgs e)
+    private async void OnFlyoutTextBoxKeyDown(object? sender, KeyEventArgs e)
     {
         if (e.Key == Key.Enter && sender is TextBox tb && DataContext is GenHotkeysViewModel vm)
         {
+            e.Handled = true;
             if (string.Equals(tb.Tag as string, "Create", StringComparison.OrdinalIgnoreCase))
             {
-                vm.CreateNewProfileCommand.Execute(null);
+                await vm.CreateNewProfileCommand.ExecuteAsync(null);
                 (this.FindControl<Button>("NewProfileButton")?.Flyout as Flyout)?.Hide();
-                e.Handled = true;
             }
             else if (string.Equals(tb.Tag as string, "Rename", StringComparison.OrdinalIgnoreCase))
             {
-                vm.RenameCurrentProfileCommand.Execute(null);
+                await vm.RenameCurrentProfileCommand.ExecuteAsync(null);
                 (this.FindControl<Button>("RenameProfileButton")?.Flyout as Flyout)?.Hide();
-                e.Handled = true;
             }
         }
     }
 
-    private void OnCloseNewProfileFlyout(object? sender, RoutedEventArgs e)
+    private async void OnCreateProfileClick(object? sender, RoutedEventArgs e)
     {
+        if (DataContext is GenHotkeysViewModel vm)
+        {
+            await vm.CreateNewProfileCommand.ExecuteAsync(null);
+        }
+
         (this.FindControl<Button>("NewProfileButton")?.Flyout as Flyout)?.Hide();
     }
 
-    private void OnCloseRenameProfileFlyout(object? sender, RoutedEventArgs e)
+    private async void OnSaveRenameProfileClick(object? sender, RoutedEventArgs e)
     {
+        if (DataContext is GenHotkeysViewModel vm)
+        {
+            await vm.RenameCurrentProfileCommand.ExecuteAsync(null);
+        }
+
         (this.FindControl<Button>("RenameProfileButton")?.Flyout as Flyout)?.Hide();
     }
 }
