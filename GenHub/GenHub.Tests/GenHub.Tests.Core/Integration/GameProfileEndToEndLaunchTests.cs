176b5a72b4098c5f259a1a2a7e9da2d19c4ed783
Bobby Battista <bobtista@gmail.com>
2026-09-15 12:51:49 -0400

    chore: adapt PR #341 to current development APIs and tests

--- GenHub/GenHub.Tests/GenHub.Tests.Core/Integration/GameProfileEndToEndLaunchTests.cs
+++ GenHub/GenHub.Tests/GenHub.Tests.Core/Integration/GameProfileEndToEndLaunchTests.cs
@@ -78,6 +78,12 @@ public class GameProfileEndToEndLaunchTests : IDisposable
         _configurationProviderServiceMock.Setup(x => x.GetApplicationDataPath()).Returns(_testTempDir);
         _configurationProviderServiceMock.Setup(x => x.GetDefaultWorkspaceStrategy()).Returns(WorkspaceStrategy.SymlinkOnly);
 
+        var receiptService = new Mock<ILaunchReceiptService>();
+        receiptService.Setup(x => x.RevalidateAsync(It.IsAny<string>(), It.IsAny<CancellationToken>()))
+            .ReturnsAsync(OperationResult<LaunchReceiptDriftReport>.CreateSuccess(new LaunchReceiptDriftReport()));
+        receiptService.Setup(x => x.RecordLaunchAsync(It.IsAny<LaunchReceiptContext>(), It.IsAny<CancellationToken>()))
+            .ReturnsAsync(OperationResult<LaunchReceipt>.CreateSuccess(new LaunchReceipt()));
+
         _gameLauncher = new GameLauncher(
             _launcherLoggerMock.Object,
             _profileManager,
@@ -92,7 +98,8 @@ public class GameProfileEndToEndLaunchTests : IDisposable
             _gameSettingsServiceMock.Object,
             _profileContentLinkerMock.Object,
             _steamLauncherMock.Object,
-            _configurationProviderServiceMock.Object);
+            _configurationProviderServiceMock.Object,
+            receiptService.Object);
     }
 
     /// <inheritdoc/>
