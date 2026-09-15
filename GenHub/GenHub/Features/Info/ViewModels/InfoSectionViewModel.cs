a7d3e7d85c19b44e26e73754aed5ca26fb526373
undead2146 <undead2146@users.noreply.github.com>
2026-09-15 03:01:44 +0000

    refactor(info): use primary constructor in InfoSectionViewModel

--- GenHub/GenHub/Features/Info/ViewModels/InfoSectionViewModel.cs
+++ GenHub/GenHub/Features/Info/ViewModels/InfoSectionViewModel.cs
@@ -1,75 +1,56 @@
+using CommunityToolkit.Mvvm.ComponentModel;
+using GenHub.Core.Interfaces.Common;
+using GenHub.Core.Models.Info;
 using System;
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
 using System.Linq;
-using CommunityToolkit.Mvvm.ComponentModel;
-using GenHub.Core.Interfaces.Common;
-using GenHub.Core.Models.Info;
 
 namespace GenHub.Features.Info.ViewModels;
 
 /// <summary>
 /// ViewModel for an info section.
 /// </summary>
-public partial class InfoSectionViewModel : ObservableObject
+public partial class InfoSectionViewModel(InfoSection model, ILocalizationService? localizationService = null) : ObservableObject
 {
-    private readonly InfoSection _model;
-    private readonly ILocalizationService? _localizationService;
+    [ObservableProperty]
+    private string _id = model.Id;
 
     [ObservableProperty]
-    private string _id;
+    private string _title = ResolveString(localizationService, $"Info.Section.{model.Id}.Title", model.Title);
 
     [ObservableProperty]
-    private string _title;
+    private string _description = ResolveString(localizationService, $"Info.Section.{model.Id}.Description", model.Description);
 
     [ObservableProperty]
-    private string _description;
-
-    [ObservableProperty]
-    private int _order;
-
-    /// <summary>
-    /// Initializes a new instance of the <see cref="InfoSectionViewModel"/> class.
-    /// </summary>
-    public InfoSectionViewModel(InfoSection model, ILocalizationService? localizationService = null)
-    {
-        _model = model;
-        _localizationService = localizationService;
-        _id = model.Id;
-        _order = model.Order;
-
-        _title = ResolveString(localizationService, $"Info.Section.{model.Id}.Title", model.Title);
-        _description = ResolveString(localizationService, $"Info.Section.{model.Id}.Description", model.Description);
-
-        Cards = new(model.Cards.Select(c => new InfoCardViewModel
-        {
-            Title = c.Title,
-            Content = c.Content,
-            Type = c.Type,
-            IsExpandable = c.IsExpandable,
-            DetailedContent = c.DetailedContent,
-            Actions = c.Actions,
-        }));
-    }
+    private int _order = model.Order;
 
     /// <summary>
     /// Gets the underlying model.
     /// </summary>
-    public InfoSection Model => _model;
+    public InfoSection Model => model;
+
+    /// <summary>
+    /// Gets the collection of cards in this section.
+    /// </summary>
+    public ObservableCollection<InfoCardViewModel> Cards { get; } = new(model.Cards.Select(c => new InfoCardViewModel
+    {
+        Title = c.Title,
+        Content = c.Content,
+        Type = c.Type,
+        IsExpandable = c.IsExpandable,
+        DetailedContent = c.DetailedContent,
+        Actions = c.Actions,
+    }));
 
     /// <summary>
     /// Notifies that localization has changed.
     /// </summary>
     public void NotifyLocalizationChanged()
     {
-        Title = ResolveString(_localizationService, $"Info.Section.{Id}.Title", _model.Title);
-        Description = ResolveString(_localizationService, $"Info.Section.{Id}.Description", _model.Description);
+        Title = ResolveString(localizationService, $"Info.Section.{Id}.Title", model.Title);
+        Description = ResolveString(localizationService, $"Info.Section.{Id}.Description", model.Description);
     }
-
-    /// <summary>
-    /// Gets the collection of cards in this section.
-    /// </summary>
-    public ObservableCollection<InfoCardViewModel> Cards { get; }
 
     private static string ResolveString(ILocalizationService? loc, string key, string fallback)
     {
