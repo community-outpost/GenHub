002624f92f6bda0d33c530ff18206e19137f3d13
undead2146 <undead2146@github.com>
2026-09-26 04:33:00 +0000

    fix(catalog): restore correct using directives in CatalogUpstreamIngestionService

--- GenHub/GenHub/Features/Content/Services/Catalog/CatalogUpstreamIngestionService.cs
+++ GenHub/GenHub/Features/Content/Services/Catalog/CatalogUpstreamIngestionService.cs
@@ -1,18 +1,22 @@
+using GenHub.Core.Constants;
+using GenHub.Core.Interfaces.Content;
+using GenHub.Core.Interfaces.GitHub;
+using GenHub.Core.Models.Content;
+using GenHub.Core.Models.Enums;
+using GenHub.Core.Models.GitHub;
+using GenHub.Core.Models.Manifest;
+using GenHub.Core.Models.Providers;
+using GenHub.Core.Models.Results.Content;
+using GenHub.Features.Content.Services.CommunityOutpost;
+using GenHub.Features.Content.Services.GeneralsOnline;
+using GenHub.Features.Content.Services.GitHub;
+using Microsoft.Extensions.Logging;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text.RegularExpressions;
 using System.Threading;
 using System.Threading.Tasks;
-using GenHub.Core.Constants;
-using GenHub.Core.Interfaces.Content;
-using GenHub.Core.Models.Catalog;
-using GenHub.Core.Models.Content;
-using GenHub.Core.Models.Enums;
-using GenHub.Core.Models.GitHub;
-using GenHub.Features.Content.Services.CommunityOutpost;
-using GenHub.Features.Content.Services.GeneralsOnline;
-using Microsoft.Extensions.Logging;
 
 namespace GenHub.Features.Content.Services.Catalog;
 
