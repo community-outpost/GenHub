8fd96c2c81da985a4c3797d4f346dd7a88129261
Bobby Battista <bobtista@gmail.com>
2026-09-25 22:25:10 -0400

    test(downloads): verify each action tooltip resolves localization

--- GenHub/GenHub.Tests/GenHub.Tests.Core/Features/Downloads/Views/DownloadsTooltipTests.cs
+++ GenHub/GenHub.Tests/GenHub.Tests.Core/Features/Downloads/Views/DownloadsTooltipTests.cs
@@ -1,7 +1,11 @@
 using Avalonia.Controls;
 using Avalonia.Headless.XUnit;
 using Avalonia.LogicalTree;
+using GenHub.Core.Constants;
+using GenHub.Core.Interfaces.Common;
 using GenHub.Features.Downloads.Views;
+using Moq;
+using System;
 using System.Linq;
 using Xunit;
 
@@ -18,7 +22,7 @@ public class DownloadsTooltipTests
     [AvaloniaFact]
     public void ContentCardView_Tooltips_AreNotRawMarkup()
     {
-        AssertNoRawMarkupTooltips(new ContentCardView());
+        AssertTooltips(() => new ContentCardView(), "DownloadButton", "UpdateButton", "AddToProfileButton");
     }
 
     /// <summary>
@@ -27,7 +31,40 @@ public class DownloadsTooltipTests
     [AvaloniaFact]
     public void ContentDetailView_Tooltips_AreNotRawMarkup()
     {
-        AssertNoRawMarkupTooltips(new ContentDetailView());
+        AssertTooltips(() => new ContentDetailView(), "UpdateButton", "AddToProfileButton", "DeleteDownloadButton", "OpenWebsiteButton");
+    }
+
+    private static void AssertTooltips(Func<Control> createView, params string[] buttonNames)
+    {
+        var resources = Avalonia.Application.Current!.Resources;
+        var hadPrevious = resources.TryGetValue(LocalizationConstants.ResourceServiceKey, out var previous);
+        var localization = new Mock<ILocalizationService>();
+        localization.Setup(service => service[It.IsAny<string>()]).Returns((string key) => $"Translated: {key}");
+        resources[LocalizationConstants.ResourceServiceKey] = localization.Object;
+        try
+        {
+            var view = createView();
+            foreach (var buttonName in buttonNames)
+            {
+                var button = view.FindControl<Button>(buttonName);
+                Assert.NotNull(button);
+                var tooltip = Assert.IsType<string>(ToolTip.GetTip(button));
+                Assert.StartsWith("Translated: Downloads.", tooltip);
+            }
+
+            AssertNoRawMarkupTooltips(view);
+        }
+        finally
+        {
+            if (hadPrevious)
+            {
+                resources[LocalizationConstants.ResourceServiceKey] = previous;
+            }
+            else
+            {
+                resources.Remove(LocalizationConstants.ResourceServiceKey);
+            }
+        }
     }
 
     private static void AssertNoRawMarkupTooltips(Control view)
